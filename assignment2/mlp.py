484105a64a0782a707fd1c3536fef56dc3a4b8bd
Madeleine Negård <madelne@stud.ntnu.no>
2025-10-14 13:37:22 +0200

    compiler, fit and evaluator

--- assignment2/mlp.py
+++ assignment2/mlp.py
@@ -1,12 +1,23 @@
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Dense
 
 class mlp():
     def __init__(self):
         self.model = Sequential([
-            Flatten(input_shape=(28, 28)),
-            Dense(256, activation='sigmoid'),  
-            Dense(128, activation='sigmoid'), 
-            Dense(10, activation='softmax'),  
+            Dense(256, activation='relu', input_shape=(2,)),  
+            Dense(128, activation='relu'), 
+            Dense(1, activation='sigmoid'),  
         ])
-
+           
+    def compile(self):
+        self.model.compile(optimizer='adam',
+              loss='binary_crossentropy',  
+              metrics=['accuracy'])
+        
+    def fit(self, X, y):
+        self.model.fit(X, y, epochs=100, 
+          batch_size=20, 
+          validation_split=0.2)
+          
+    def evaluate(self, X, y):
+        self.model.evaluate(X, y, verbose=0)
