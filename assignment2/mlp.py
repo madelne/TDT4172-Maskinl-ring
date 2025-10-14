900e0ea7f93c0b0cc7255c1a73e311ba66fe0ca7
Madeleine Negård <madelne@stud.ntnu.no>
2025-10-14 12:58:14 +0200

    import tensorflow

--- assignment2/mlp.py
+++ assignment2/mlp.py
@@ -1,1 +1,12 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+
+class mlp():
+    def __init__(self):
+        self.model = Sequential([
+            Flatten(input_shape=(28, 28)),
+            Dense(256, activation='sigmoid'),  
+            Dense(128, activation='sigmoid'), 
+            Dense(10, activation='softmax'),  
+        ])
 
